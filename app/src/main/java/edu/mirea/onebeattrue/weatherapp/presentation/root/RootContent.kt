227cc922949a45826f21deb61f608a86153a2d25
Vitaly <onebeattrue@gmail.com>
2024-02-28 18:54:40 +0300

    Создал структуру presentation слоя (component, store, content)

---
+++ app/src/main/java/edu/mirea/onebeattrue/weatherapp/presentation/root/RootContent.kt
@@ -0,0 +1,12 @@
+package edu.mirea.onebeattrue.weatherapp.presentation.root
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun RootContent(
+    modifier: Modifier = Modifier,
+    component: RootComponent
+) {
+
+}
