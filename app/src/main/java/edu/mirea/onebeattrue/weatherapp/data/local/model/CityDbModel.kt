840109e7ee4a265b12d7a4749729ad2c49390bbc
Vitaly <onebeattrue@gmail.com>
2024-02-29 18:07:58 +0300

    Реализовал FavouriteDatabase

---
+++ app/src/main/java/edu/mirea/onebeattrue/weatherapp/data/local/model/CityDbModel.kt
@@ -0,0 +1,11 @@
+package edu.mirea.onebeattrue.weatherapp.data.local.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "favourite_cities")
+data class CityDbModel(
+    @PrimaryKey val id: Int,
+    val name: String,
+    val country: String
+)
